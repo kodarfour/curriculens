4984a24799ffd89b98665d05194d3d1cb43bb105
zohaibk04 <zohaibk1204@gmail.com>
2025-01-29 20:53:24 -0500

    Finished method to get the 4 most recent terms

--- python-backend/main.py
+++ python-backend/main.py
@@ -1,6 +1,25 @@
-#PYTHON 3.12.2
 import requests
 import json
+import datetime
+
+def get_recent_terms():
+    now = datetime.datetime.now()
+    month = int(now.strftime("%m"))
+    day = int(now.strftime("%d")) # in case we need it
+    year = int(now.strftime("%y"))
+
+    if month < 4 or month > 11: # Might need to change this depending on when new classes exactly release
+        seasons = ["2", "8", "2", "8"] # 2 means Spring semester, 8 means Fall
+        years = [year, year-1, year-1, year-2]
+    else:
+        seasons = ["8", "2", "8", "2"]
+        years = [year, year, year-1, year-1]
+
+    terms = []
+    for i in range(len(seasons)):
+        terms.append("1" + str(years[i]) + str(seasons[i])) # terms formula is “1” + [2 digit year] + [2 for Spring, 8 for Fall
+    
+    return terms
 
 def get_courses():
     base_url = 'https://sisuva.admin.virginia.edu/psc/ihprd/UVSS/SA/s/WEBLIB_HCX_CM.H_CLASS_SEARCH.FieldFormula.IScript_ClassSearch?institution=UVA01'
@@ -8,5 +27,9 @@ def get_courses():
     response = requests.get(base_url + '&subject=' + course[0] + '&catalog_nbr=' + course[1])
     return response
 
-response = get_courses()
-print(response.json())
+
+
+# response = get_courses()
+# print(response.json())
+
+print(get_recent_terms())
